15a643fce292a192f8dde7a87dfd351438f557fb
Mark Clearwater <csmacnz@csmac.co.nz>
2016-08-12 23:34:12 +0100

    More TinyString Tests

--- test/dotnetcli/t4tinytypes.sample.tests/TinyStringTests.cs
+++ test/dotnetcli/t4tinytypes.sample.tests/TinyStringTests.cs
@@ -9,6 +9,15 @@ namespace Tests
         public void TinyStringExists() 
         {
             var tinyString = new t4tinytypes.sample.TinyString();
+
+            Assert.NotNull(tinyString);
+        }
+
+        [Fact]
+        public void TinyStringToStringMatches() 
+        {
+            var tinyString = new t4tinytypes.sample.TinyString("Watsit");
+            Assert.Equal("Watsit", tinyString.ToString());
         }
 
         [Fact]
@@ -26,7 +35,11 @@ namespace Tests
             var tinyString1 = new t4tinytypes.sample.TinyString("Hello");
             var tinyString2 = new t4tinytypes.sample.TinyString("Hello");
             
-            Assert.Equal(tinyString1, tinyString2); 
+            Assert.Equal(tinyString1, tinyString2);
+            Assert.True(tinyString1.Equals(tinyString2)); 
+            Assert.True(tinyString1.Equals((object)tinyString2)); 
+            Assert.True(tinyString2.Equals(tinyString1)); 
+            Assert.True(tinyString2.Equals((object)tinyString1)); 
         }
 
         [Fact]
@@ -35,7 +48,11 @@ namespace Tests
             var tinyString1 = new t4tinytypes.sample.TinyString("Hello");
             var tinyString2 = new t4tinytypes.sample.TinyString("World");
             
-            Assert.NotEqual(tinyString1, tinyString2); 
+            Assert.NotEqual(tinyString1, tinyString2);
+            Assert.False(tinyString1.Equals(tinyString2)); 
+            Assert.False(tinyString1.Equals((object)tinyString2)); 
+            Assert.False(tinyString2.Equals(tinyString1)); 
+            Assert.False(tinyString2.Equals((object)tinyString1));
         }
     }
 }
